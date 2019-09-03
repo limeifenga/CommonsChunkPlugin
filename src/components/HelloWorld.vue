630bad9cc4bcb3311cfa857306476fdd0108d68a
limeifeng <>
2019-09-03 18:11:48 +0800

    CommonsChunkPlugin

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    helloworld
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/b.css');
+@import '../assets/css/a.css';
+</style>
